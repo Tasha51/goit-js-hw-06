66378552ab8de6669bec854b413f0e58343c22b3
Nataliia Tatsiienko <natasha51star29@gmail.com>
2022-07-03 12:42:14 +0300

    add task№2

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+
 const categoryItemEl = document.querySelectorAll('.item');
 console.log('Number of categories:', categoryItemEl.length);
 
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +14,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = ingredients.map((elem, i) => {
+ const itemEl = document.createElement('li');
+ itemEl.textContent = `${ingredients[i]}`;
+ itemEl.classList.add('item');
+  
+  return itemEl;
+})
+console.log(elements);
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...elements);
